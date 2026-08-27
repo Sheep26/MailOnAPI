fd95c904217eff9aed8f55b610dd588f6ac59f82
Sheep26 <drew@drewfitzgerald.co.nz>
2026-08-27 15:09:55 +1200

    rah

--- database/database.js
+++ database/database.js
@@ -93,11 +93,15 @@ export class DatabaseManager {
         await db.execute("UPDATE users SET mail_boxes=JSON_ARRAY_APPEND(mail_boxes, '$', ?) WHERE email=?", [mail_box, email])
     }
 
-    async deleteMailBox(email, mail_box) {
+    async deleteMailBoxName(email, mail_box) {
         if (mail_box.toLowerCase() == "inbox" || mail_box.toLowerCase() == "sent")
             return;
 
         await db.execute("UPDATE users SET mail_boxes=JSON_REMOVE(mail_boxes, REPLACE(JSON_SEARCH(mail_boxes, 'one', ?))) WHERE JSON_SEARCH(mail_boxes, 'one', ?) IS NOT NULL AND email=?", [mail_box, mail_box, email]);
+    }
+
+    async deleteMailBox(email, mail_box) {
+        await db.execute("UPDATE users SET mail_boxes=JSON_REMOVE(mail_boxes, JSON_UNQUOTE(JSON_SEARCH(json_column, 'one', ?, NULL, '$[*].id'))) WHERE JSON_SEARCH(json_column, 'one', ?, NULL, '$[*].id') IS NOT NULL AND email=?", [mail_box, mail_box, email]);
     }
 
     async markSeen(mail_id, email) {
