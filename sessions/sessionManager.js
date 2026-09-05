d841ecb9f993ae7d1e2348c1f14a929c98f48cf0
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-05 16:25:02 +1200

    Crypto stuff

--- sessions/sessionManager.js
+++ sessions/sessionManager.js
@@ -1,18 +1,7 @@
+import crypto from 'node:crypto';
+
 function randStr(len) {
-    /*
-    * This function generates a random string with length provided.
-    
-    * @param len - The length of the strign;
-    */
-
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    let result = '';
-    for (let i = 0; i < len; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    return result;
+    return crypto.randomBytes(len).toString('base64url').slice(0, len);
 }
 
 export class SessionManager {
