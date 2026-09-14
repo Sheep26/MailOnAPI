f65be1a17b0f0dfa60137b4a2c3ddf9d5a779184
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-14 22:53:17 +1200

    Fix that

--- email/resend.js
+++ email/resend.js
@@ -89,7 +89,7 @@ export class EmailResend extends Email {
         const mailbox = await this.database.getMailBox(user.email, 'Inbox');
 
         if (mailbox) {
-            this.database.addEmail(data.to[0], data.to[0], data.headers.from, data.headers['return-path'], JSON.stringify(data.bcc), JSON.stringify(data.cc), data.id, data.message_id, data.html_format, data.subject, data.html, data.attachments, references, mailbox.uid);
+            await this.database.addEmail(data.to[0], data.to[0], data.headers.from, data.headers['return-path'], JSON.stringify(data.bcc), JSON.stringify(data.cc), data.id, data.message_id, data.html_format, data.subject, data.html, data.attachments, references, mailbox.uid);
 
             super.updateImap(user.email, mailbox.uid);
         }
