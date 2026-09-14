67635a862dd7a98a089a8648403e3beed58020c3
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-14 22:48:39 +1200

    Fuck me

--- imap/imapServer.js
+++ imap/imapServer.js
@@ -4,7 +4,7 @@ import { ImapConnection } from "./imapConnection.js";
 let connections = [];
 
 export function imapHandleRecieved(belongs_to, exists, updated_mailbox) {
-    const filtered_connections = connections.filter(connection => connection.user.email == belongs_to, connection.active, connection.idle, connection.mailbox.uid == updated_mailbox);
+    const filtered_connections = connections.filter(connection => connection.user.email == belongs_to && connection.active && connection.idle && connection.mailbox.uid == updated_mailbox);
 
     for (let connection of filtered_connections)
         connection.send(`* ${exists} EXISTS`);
