db8a39302c73882fab05d89d406684b7ee83c37e
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-14 22:33:55 +1200

    Fix store

--- imap/commands/store.js
+++ imap/commands/store.js
@@ -12,21 +12,22 @@ export class StoreCommand extends Command {
         const uid = options.uid ?? false;
         const sequence = args[0];
         const action = args[1];
+        const flags = args[2];
 
         const emails = await this.database.getUsersEmails(this.connection.user.email);
         const mailboxEmails = emails.filter(email => email.mail_box === this.connection.mailbox.uid).sort((a, b) => a.uid - b.uid);
         const selectedMails = this.selectEmails(mailboxEmails, sequence, uid);
 
         for (let email of selectedMails) {
-            if (action.startsWith("+Flags"))
-                for (let flag of args[2])
-                    this.database.addFlag(email.mail_id, this.connection.user.email, flag);
-
-            if (action.startsWith("-Flags"))
-                for (let flag of args[2])
-                    await this.database.removeFlag(email.mail_id, this.connection.user.email, flag);
+            for (let flag of flags)
+                if (action.toUpperCase().startsWith("+FLAGS"))
+                    if (!email.flags.includes(flag))
+                        await this.database.addFlag(email.mail_id, this.connection.user.email, flag);
+                else if (action.toUpperCase().startsWith("-FLAGS"))
+                    if (email.flags.includes(flag))
+                        await this.database.removeFlag(email.mail_id, this.connection.user.email, flag);
             
-            if (!action.endsWith(".SILENT")) {
+            if (!action.toUpperCase().endsWith(".SILENT")) {
                 const email_updated = await this.database.getEmail(email.mail_id, this.connection.user.email);
                 const email_sequence = await this.connection.database.getEmailIndex(this.connection.user.email, email.mail_id, email.mail_box);
                 
